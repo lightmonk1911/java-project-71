327188a0278a6bf440d77d231d9511831b3a5cb1
Igor Babenkov <igor.babenkov@corp.mail.ru>
2022-11-05 07:18:59 +0300

    feat: step3 - cli connect

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,21 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "0.1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<String> {
+    @Override
+    public String call() {
+        System.out.println("Hello, world!");
+        return "";
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
