075c573a94a6c528529dc3e8ff5b68f5e26e3197
Igor Babenkov <igor.babenkov@corp.mail.ru>
2022-11-05 07:36:52 +0300

    feat: step4 add params: paths and option: format

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,16 +1,30 @@
 package hexlet.code;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "0.1.0",
         description = "Compares two configuration files and shows a difference.")
 class App implements Callable<String> {
+
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: ${DEFAULT-VALUE}]", defaultValue = "stylish")
+    String format;
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    File filepath1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    File filepath2;
+
     @Override
     public String call() {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world! filepath1 = " + filepath1 + " filepath2 = " + filepath2);
+        System.out.println("format = " + format);
         return "";
     }
 
