c24a2182baa50eb34b49f5982fbee07952147096
Igor Babenkov <igor.babenkov@corp.mail.ru>
2022-11-05 13:22:50 +0300

    fix: lint

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
 
